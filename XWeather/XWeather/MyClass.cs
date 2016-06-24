a4b1bcec19bd2cf7055175e857a287e06042a0a3
Colby Williams <colbylwilliams@gmail.com>
2016-06-24 14:32:46 -0400

    initial mobile project creation

---
+++ XWeather/XWeather/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace XWeather
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ XWeather/iOS/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using UIKit;
+
+namespace XWeather.iOS
+{
+	public partial class ViewController : UIViewController
+	{
+		int count = 1;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Code to start the Xamarin Test Cloud Agent
+#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start ();
+#endif
+
+			// Perform any additional setup after loading the view, typically from a nib.
+			Button.AccessibilityIdentifier = "myButton";
+			Button.TouchUpInside += delegate {
+				var title = string.Format ("{0} clicks!", count++);
+				Button.SetTitle (title, UIControlState.Normal);
+			};
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.		
+		}
+	}
+}
