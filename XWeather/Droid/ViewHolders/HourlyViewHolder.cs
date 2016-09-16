60631c66afd94ad19292e1e766e516e56cd2abc0
Colby Williams <colbylwilliams@gmail.com>
2016-09-16 14:46:35 -0400

    android style updates

--- XWeather/Droid/ViewHolders/HourlyViewHolder.cs
+++ XWeather/Droid/ViewHolders/HourlyViewHolder.cs
@@ -13,6 +13,7 @@ namespace XWeather.Droid
 	public class HourlyViewHolder : ViewHolder<HourlyForecast>
 	{
 		TextView hourLabel;
+		TextView periodLabel;
 		TextView tempLabel;
 		TextView precipLabel;
 		ImageView iconImageView;
@@ -32,6 +33,8 @@ namespace XWeather.Droid
 		public override void FindViews (View rootView)
 		{
 			hourLabel = (TextView)rootView.FindViewById (Resource.Id.HourlyListItem_hourLabel);
+			periodLabel = (TextView)rootView.FindViewById (Resource.Id.HourlyListItem_periodLabel);
+
 			tempLabel = (TextView)rootView.FindViewById (Resource.Id.HourlyListItem_tempLabel);
 			precipLabel = (TextView)rootView.FindViewById (Resource.Id.HourlyListItem_precipLabel);
 			iconImageView = (ImageView)rootView.FindViewById (Resource.Id.HourlyListItem_iconImageView);
@@ -43,6 +46,8 @@ namespace XWeather.Droid
 		public override void SetData (HourlyForecast data)
 		{
 			hourLabel.SetText (data.HourString (), TextView.BufferType.Normal);
+			periodLabel.SetText (data.PeriodString (), TextView.BufferType.Normal);
+
 			tempLabel.SetText (data.TempString (Settings.UomTemperature, true, true), TextView.BufferType.Normal);
 			precipLabel.SetText (data.ProbabilityPercipString (), TextView.BufferType.Normal);
 			iconImageView.SetImageResource (ImageProvider.ResourceForString (data.icon));
